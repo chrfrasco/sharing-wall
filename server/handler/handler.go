05d3368102c34219d80c73b04143f8e271252e90
Christian Scott <christian@christianfscott.com>
2018-04-05 17:30:21 +1200

    Refactor handler

--- server/handler/handler.go
+++ server/handler/handler.go
@@ -3,6 +3,8 @@ package handler
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"strconv"
 
@@ -12,17 +14,44 @@ import (
 	"github.com/chrfrasco/sharing-wall/server/storage"
 )
 
-type Handler struct {
+type handler struct {
 	svc storage.Service
 }
 
-// New creates a handler instance
-func New(svc storage.Service) *Handler {
-	return &Handler{svc}
+// New creates a handler instance for the quote service
+func New(svc storage.Service) http.Handler {
+	mux := http.NewServeMux()
+	h := handler{svc}
+	mux.HandleFunc("/api/message", responseHandler(h.message))
+	mux.HandleFunc("/api/quotes", responseHandler(h.quotes))
+	return mux
 }
 
-// Message sends a json-encoded message based on the client's language settings
-func (h *Handler) Message(w http.ResponseWriter, r *http.Request) {
+func responseHandler(h func(io.Writer, *http.Request) (interface{}, int, error)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, status, err := h(w, r)
+		if err != nil {
+			data = err.Error()
+		}
+
+		if status == http.StatusInternalServerError {
+			data = "Sorry - something went wrong"
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(status)
+		err = json.NewEncoder(w).Encode(data)
+		if err != nil {
+			log.Printf("could not encode response to output: %v", err)
+		}
+	}
+}
+
+func (h handler) message(w io.Writer, r *http.Request) (interface{}, int, error) {
+	if r.Method != http.MethodGet {
+		return nil, http.StatusMethodNotAllowed, nil
+	}
+
 	var msg string
 	switch tag := lang.FromRequest(r); tag {
 	case language.English:
@@ -31,32 +60,29 @@ func (h *Handler) Message(w http.ResponseWriter, r *http.Request) {
 		msg = "Hallo von Golang"
 	}
 
-	sendJSON(w, fmt.Sprintf(`{ "msg": "%s" }`, msg))
+	return msg, http.StatusOK, nil
 }
 
-func (h *Handler) Quotes(w http.ResponseWriter, r *http.Request) {
+func (h handler) quotes(w io.Writer, r *http.Request) (interface{}, int, error) {
+	if r.Method != http.MethodGet {
+		return nil, http.StatusMethodNotAllowed, fmt.Errorf("Method %s not allowed", r.Method)
+	}
+
 	limitStr := r.URL.Query().Get("limit")
 	limit, err := strconv.Atoi(limitStr)
 	if err != nil {
 		limit = 10
 	}
 
+	if limit < 0 {
+		return nil, http.StatusBadRequest, fmt.Errorf("Negative limit")
+	}
+
 	quotes, err := h.svc.ListQuotes(limit)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		log.Printf("could not retrieve from database: %v\n", err)
+		return nil, http.StatusInternalServerError, nil
 	}
 
-	jsonStr, err := json.Marshal(quotes)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	sendJSON(w, string(jsonStr))
-}
-
-func sendJSON(w http.ResponseWriter, json string) {
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprint(w, json)
+	return quotes, http.StatusOK, nil
 }
