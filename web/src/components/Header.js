98d90106808faac89f6d03a1de55033290f3742c
Christian Scott <christianfscott@gmail.com>
2018-04-11 15:16:09 +1200

    Add fetch to header for testing

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,9 +1,39 @@
 import React from "react";
+import api from "../api";
 
-export default function Header() {
-  return (
-    <header className="App-header">
-      <h1>Hello</h1>
-    </header>
-  );
+const LOADING = Symbol("LOADING");
+const LOADED = Symbol("LOADED");
+const ERROR = Symbol("ERROR");
+
+export default class Header extends React.Component {
+  state = { fetchState: LOADING };
+
+  componentDidMount() {
+    api
+      .getMessage()
+      .then(msg => this.setState({ fetchState: LOADED, msg }))
+      .catch(() => this.setState({ fetchState: ERROR }));
+  }
+
+  render() {
+    let msg;
+    switch (this.state.fetchState) {
+      case LOADING:
+        msg = "Loading...";
+        break;
+      case LOADED:
+        msg = this.state.msg;
+        break;
+      case ERROR:
+        msg = "Something went wrong";
+        break;
+      default:
+        throw new TypeError(`could not handle state ${this.state.fetchState}`);
+    }
+    return (
+      <header className="App-header">
+        <h1>{msg}</h1>
+      </header>
+    );
+  }
 }
