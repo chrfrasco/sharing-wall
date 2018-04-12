0cdfa7d8fb2bf20dc8b888f113420ce9d0233d2c
Christian Scott <christianfscott@gmail.com>
2018-04-12 16:28:47 +1200

    Remove header

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
-import Header from "./components/Header";
 import Page from "./components/Page";
 import Home from "./views/Home";
 
@@ -8,8 +7,6 @@ class App extends Component {
   render() {
     return (
       <Page>
-        <Header />
-
         <Switch>
           <Route exact path="/" component={Home} />
         </Switch>
