f77d906e2d513abd282fdef3679fad850ac5f4a8
Christian Scott <christianfscott@gmail.com>
2018-04-12 20:37:15 +1200

    Add image generation service

---
+++ img/index.js
@@ -0,0 +1,69 @@
+//@ts-check
+const bodyParser = require("body-parser");
+const express = require("express");
+const morgan = require("morgan");
+
+const createRenderer = require("./src/renderer");
+const uploader = require("./src/uploader");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(morgan("combined"));
+
+function validate(body, properties) {
+  let valid = true;
+  let missing = [];
+  for (const prop of properties) {
+    if (body[prop] == null || body[prop] == "") {
+      valid = false;
+      missing.push(prop);
+    }
+  }
+
+  const message = `Missing ${missing.map(s => `"${s}"`).join(", ")} in body`;
+  return { valid, message };
+}
+
+let renderer;
+
+async function handle(req, res, { quote = null, name = null }) {
+  const { valid, message } = validate({ quote, name }, ["quote", "name"]);
+  if (!valid) {
+    res.status(400).send(message);
+    return;
+  }
+
+  const imgBuf = await renderer.quote({ quote, name });
+  const img = `data:image/png;base64,${imgBuf.toString("base64")}`;
+  res.status(200).send(img);
+}
+
+app.get("/", async (req, res) => {
+  await handle(req, res, req.query);
+});
+
+app.post("/", async (req, res) => {
+  await handle(req, res, req.body);
+});
+
+app.use((err, req, res, next) => {
+  // eslint-disable-next-line no-console
+  console.error(err);
+  next(err);
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  res.status(500).json({ msg: "something went wrong" });
+});
+
+createRenderer()
+  .then(createdRenderer => {
+    renderer = createdRenderer;
+    // eslint-disable-next-line no-console
+    app.listen(port, () => console.log(`app is listening on :${port}`));
+  })
+  // eslint-disable-next-line no-console
+  .catch(e => console.error(`failed to init browser: ${e}`));
