0af2fe4bd21ae488f97a179526b4c16952b042ac
Christian Scott <christianfscott@gmail.com>
2018-04-13 16:26:01 +1200

    Remove request in header

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,35 +1,8 @@
 import React from "react";
-import api, { states } from "../api";
+import { Link } from "react-router-dom";
 
-export default class Header extends React.Component {
-  state = { fetchState: states.LOADING };
-
-  componentDidMount() {
-    api
-      .getMessage()
-      .then(msg => this.setState({ fetchState: states.LOADED, msg }))
-      .catch(() => this.setState({ fetchState: states.ERROR }));
-  }
-
-  render() {
-    let msg;
-    switch (this.state.fetchState) {
-      case states.LOADING:
-        msg = "Loading...";
-        break;
-      case states.LOADED:
-        msg = this.state.msg;
-        break;
-      case states.ERROR:
-        msg = "Something went wrong";
-        break;
-      default:
-        throw new TypeError(`could not handle state ${this.state.fetchState}`);
-    }
-    return (
-      <header className="App-header">
-        <h1>{msg}</h1>
-      </header>
-    );
-  }
-}
+export default () => (
+  <nav>
+    <Link to="/">Go Home</Link>
+  </nav>
+);
