54f45f8d66b2ca58dd4e52d9767d151f9d2239ed
Christian Scott <christianfscott@gmail.com>
2018-05-04 13:44:01 +1200

    Add modal close button

--- web/src/components/QuoteList.js
+++ web/src/components/QuoteList.js
@@ -1,8 +1,10 @@
 import React from "react";
+import styled from "styled-components";
 import { QuoteView as Quote } from "./Quote";
 import { states } from "../api";
-import { Grid, GridItem, Spinner, QuoteBackground } from "./elements";
+import { Grid, GridItem, Spinner, QuoteBackground, Heading } from "./elements";
 import Modal from "./Modal";
+import close from "./close.svg";
 
 export default function QuoteList({ quotes, loadState }) {
   switch (loadState) {
@@ -20,20 +22,54 @@ function QuoteListItem({ quote }) {
     <Modal>
       <Modal.Toggle>
         {openModal => (
-          <GridItem onClick={openModal}>
+          <GridItem style={{ cursor: "pointer" }} onClick={openModal}>
+            <QuoteBackground>
             <Quote quote={quote} />
+            </QuoteBackground>
           </GridItem>
         )}
       </Modal.Toggle>
 
       <Modal.Body>
-        {() => (
-          <QuoteBackground>
-            <Quote quote={quote} />
-          </QuoteBackground>
+        {({ closeModal }) => (
+          <QuotePopup quote={quote} closeModal={closeModal} />
         )}
       </Modal.Body>
     </Modal>
+  );
+}
+
+function QuotePopup({ closeModal, quote }) {
+  return (
+    <React.Fragment>
+      <CloseButton onClick={closeModal} />
+          <QuoteBackground>
+            <Quote quote={quote} />
+          </QuoteBackground>
+    </React.Fragment>
+  );
+}
+
+const CloseButtonImg = styled.img`
+  position: absolute;
+  top: 1.2rem;
+  right: 1.2rem;
+  height: 1.8rem;
+  width: 1.8rem;
+  z-index: 2;
+  cursor: pointer;
+
+  opacity: 1;
+  transition: opacity 125ms ease-in-out;
+
+  &:hover {
+    opacity: 0.4;
+  }
+`;
+
+function CloseButton({ onClick }) {
+  return (
+    <CloseButtonImg src={close} onClick={onClick} alt="close" role="button" />
   );
 }
 
