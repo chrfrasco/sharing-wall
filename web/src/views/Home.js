7faa3371ae86ee946d6e28b692a11d4093dd67e4
Christian Scott <christianfscott@gmail.com>
2018-05-25 00:14:42 +1200

    Fix/refactor form (#14)
    * Move async behaviour into fetcher
    
    * Remove duplication in QuoteSubmit case/switch
    
    * Remove unused bgVersion & refactor getQuoteFromState

--- web/src/views/Home.js
+++ web/src/views/Home.js
@@ -1,6 +1,16 @@
 import React from "react";
+import * as Fetchers from "../fetchers";
 import QuoteSubmissionForm from "../components/QuoteSubmissionForm";
 
 export default function Home() {
-  return <QuoteSubmissionForm />;
+  return (
+    <Fetchers.SubmitQuote>
+      {({ isSubmitting, submitQuote }) => (
+        <QuoteSubmissionForm
+          isSubmitting={isSubmitting}
+          submitQuote={submitQuote}
+        />
+      )}
+    </Fetchers.SubmitQuote>
+  );
 }
